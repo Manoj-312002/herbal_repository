46130d38acc7afe1dde47fccbff571ed162c2974
Manoj-312002 <52353816+Manoj-312002@users.noreply.github.com>
2020-03-30 18:55:39 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,10 +1,11 @@
 <?php
     session_start();
     // $mysqli = new mysqli("SG-Manoj-2076-master.servers.mongodirector.com", "<user>", "<password>", "<your-database-name", 3306);
-    $servername = "SG-Manoj-2076-master.servers.mongodirector.com";
-    $username = "manoj";
-    $password = "Abcdef123$";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
     $dbname = "repository";
+
     $conn = new mysqli($servername, $username, $password, $dbname,3306);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
