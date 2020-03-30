bd0812a817cc82c9e2a300ee195ec78a336d285b
Manoj-312002 <52353816+Manoj-312002@users.noreply.github.com>
2020-03-30 19:24:25 +0530

    removed

--- header.php
+++ header.php
@@ -1,6 +1,5 @@
 <?php
     session_start();
-    // $mysqli = new mysqli("SG-Manoj-2076-master.servers.mongodirector.com", "<user>", "<password>", "<your-database-name", 3306);
     $servername = "localhost";
     $username = "root";
     $password = "";
